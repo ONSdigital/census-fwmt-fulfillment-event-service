f4417025f7cc0b0103bee1c8caa479cb219d9b48
SteveScorfield <steve.scorfield@ons.gov.uk>
2021-03-17 10:17:07 +0000

    Have updated the code to check for a cancel before processing fulfilment

--- src/main/java/uk/gov/ons/census/fwmt/fulfilment/service/FulfilmentService.java
+++ src/main/java/uk/gov/ons/census/fwmt/fulfilment/service/FulfilmentService.java
@@ -7,9 +7,11 @@ import uk.gov.ons.census.fwmt.common.data.fulfillment.dto.PauseOutcome;
 import uk.gov.ons.census.fwmt.common.rm.dto.ActionInstructionType;
 import uk.gov.ons.census.fwmt.common.rm.dto.FwmtActionInstruction;
 import uk.gov.ons.census.fwmt.events.component.GatewayEventManager;
+import uk.gov.ons.census.fwmt.fulfilment.data.GatewayCache;
 import uk.gov.ons.census.fwmt.fulfilment.lookup.PauseRulesLookup;
 import uk.gov.ons.census.fwmt.fulfilment.rabbit.MessagePublisher;
 
+import javax.transaction.Transactional;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.Date;
@@ -17,11 +19,14 @@ import java.util.Locale;
 
 @Slf4j
 @Service
+@Transactional
 public class FulfilmentService {
 
   private static final String PAUSE_PROCESSED_AND_SENT = "PAUSE_PROCESSED_AND_SENT";
 
   private static final String NO_RECORD_FOUND = "NO_RECORD_FOUND";
+
+  private static final String CASE_ALREADY_CANCELLED = "CASE_ALREADY_CANCELLED";
 
   private static final String UNRECOGNISED_FULFILLMENT_CODE = "UNRECOGNISED_FULFILLMENT_CODE";
 
@@ -41,20 +46,29 @@ public class FulfilmentService {
     String caseId;
     String productCode = pauseRequest.getPayload().getFulfilmentRequest().getFulfilmentCode();
 
-    final String caseCache = cacheService.getByIdAndTypeAndExists(pauseRequest.getPayload().getFulfilmentRequest().getCaseId(),
+    final GatewayCache caseCache = cacheService.getByIdAndTypeAndExists(pauseRequest.getPayload().getFulfilmentRequest().getCaseId(),
         10, true);
-    final String indCache = cacheService.getByIndividualCaseIdAndTypeAndExists(pauseRequest.getPayload().getFulfilmentRequest().getIndividualCaseId(),
+    final GatewayCache indCache = cacheService.getByIndividualCaseIdAndTypeAndExists(pauseRequest.getPayload().getFulfilmentRequest().getIndividualCaseId(),
         10, true);
 
     if (caseCache == null && indCache == null) {
       caseId = pauseRequest.getPayload().getFulfilmentRequest().getCaseId();
       eventManager.triggerEvent(caseId, "Could not find an existing record or case is not a household", NO_RECORD_FOUND);
     } else if (caseCache == null) {
-      caseId = indCache;
-      sendPause(pauseRequest, messageReceivedTime, caseId, productCode);
+      caseId = indCache.individualCaseId;
+      if ("CANCEL".equals(indCache.lastActionInstruction) || "CANCEL(HELD)".equals(indCache.lastActionInstruction)) {
+        eventManager.triggerEvent(caseId, CASE_ALREADY_CANCELLED);
+      } else {
+        sendPause(pauseRequest, messageReceivedTime, caseId, productCode);
+      }
     } else {
-      caseId = caseCache;
-      sendPause(pauseRequest, messageReceivedTime, caseId, productCode);
+      caseId = caseCache.caseId;
+      if ("CANCEL".equals(caseCache.lastActionInstruction) || "CANCEL(HELD)".equals(caseCache.lastActionInstruction)) {
+        caseId = pauseRequest.getPayload().getFulfilmentRequest().getCaseId();
+        eventManager.triggerEvent(caseId, CASE_ALREADY_CANCELLED);
+      } else {
+        sendPause(pauseRequest, messageReceivedTime, caseId, productCode);
+      }
     }
   }
 
